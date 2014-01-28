8ad662506b12090ce0456c86cab70c44c73a7fed
Alex Nauda <alex@alexnauda.com>
2014-01-28 13:54:34 -0500

    example / test of jsonMapping feature

--- GFTestApp/GitHub Model/GitHubComment.h
+++ GFTestApp/GitHub Model/GitHubComment.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 
 @interface GitHubComment : NSObject
+@property (strong, nonatomic) NSNumber *gistId;
 @property (strong, nonatomic) NSString *body;
 @end
--- GFTestApp/GitHub Model/GitHubComment.m
+++ GFTestApp/GitHub Model/GitHubComment.m
@@ -2,4 +2,11 @@
 
 @implementation GitHubComment
 
+- (NSDictionary*)jsonMapping {
+    // you only need a jsonMapping if your property names don't match the JSON keys
+    return @{
+             @"id": @"gistId"
+             };
+}
+
 @end
